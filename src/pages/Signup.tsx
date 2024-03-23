6c081bb4bcea2bdc373ce2e91600dd2d090d84b6
eabra <emab21@student.bth.se>
2024-03-23 12:43:41 +0100

    Finish Auth Form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,26 @@
+import { useRef } from "react";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
 export function Signup() {
-  return <h1>Signup</h1>;
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const imageUrlRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-8 text-center">Sign Up</h1>
+      <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+        <label htmlFor="userName">Username</label>
+        <Input id="userName" pattern="\S*" required ref={userNameRef} />
+        <label htmlFor="name">Name</label>
+        <Input id="name" ref={nameRef} required />
+        <label htmlFor="imageUrl">Image Url</label>
+        <Input id="imageUrl" ref={imageUrlRef} type="url" />
+        <Button type="submit" className="col-span-full">
+          Sign Up
+        </Button>
+      </form>
+    </>
+  );
 }
