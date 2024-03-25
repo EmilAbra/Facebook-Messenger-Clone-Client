0116cc683428ad3afe92c0fd2bfb6298793f0d4f
eabra <emab21@student.bth.se>
2024-03-25 11:43:49 +0100

    Finish Auth

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,24 +1,48 @@
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useAuth } from "../context/AuthContext";
 
 export function Signup() {
+  const { signup } = useAuth();
   const userNameRef = useRef<HTMLInputElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
   const imageUrlRef = useRef<HTMLInputElement>(null);
 
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (signup.isPending) return;
+
+    const userName = userNameRef.current?.value;
+    const name = nameRef.current?.value;
+    const imageUrl = imageUrlRef.current?.value;
+
+    if (userName == null || userName == "" || name == null || name == "") {
+      return;
+    }
+
+    signup.mutate({ id: userName, name, image: imageUrl });
+  }
+
   return (
     <>
       <h1 className="text-3xl font-bold mb-8 text-center">Sign Up</h1>
-      <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+      <form
+        onSubmit={handleSubmit}
+        className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end"
+      >
         <label htmlFor="userName">Username</label>
         <Input id="userName" pattern="\S*" required ref={userNameRef} />
         <label htmlFor="name">Name</label>
         <Input id="name" ref={nameRef} required />
         <label htmlFor="imageUrl">Image Url</label>
         <Input id="imageUrl" ref={imageUrlRef} type="url" />
-        <Button type="submit" className="col-span-full">
-          Sign Up
+        <Button
+          disabled={signup.isPending}
+          type="submit"
+          className="col-span-full"
+        >
+          {signup.isPending ? "Loading.." : "Sign Up"}
         </Button>
       </form>
     </>
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import { AuthLayout } from "./pages/layouts/AuthLayout";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import { AuthProvider } from "./context/AuthContext";
+import { Home } from "./pages/Home";
+import { RootLayout } from "./pages/layouts/RootLayout";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <AuthLayout />,
+    element: <ContextWrapper />,
     children: [
-      { path: "login", element: <Login /> },
-      { path: "signup", element: <Signup /> },
+      {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+          { index: true, element: <Home /> },
+          {
+            path: "channel",
+            children: [
+              {
+                path: "new",
+                element: <h1>New Channel</h1>,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          { path: "login", element: <Login /> },
+          { path: "signup", element: <Signup /> },
+        ],
+      },
     ],
   },
 ]);
+
+function ContextWrapper() {
+  return (
+    <AuthProvider>
+      <Outlet />
+    </AuthProvider>
+  );
+}
